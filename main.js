0f106398138516c4916e8e4ded5b256451d4943a
Alexander Aspman <aspmanalexander@gmail.com>
2019-08-22 23:17:46 +0200

    Add files via upload
    fick inte att pusha

---
+++ main.js
@@ -0,0 +1,29 @@
+function checkForVisibility() {
+    var headers = document.querySelectorAll(".delay");
+    headers.forEach(function(header) {
+
+        
+      if (isElementInViewport(header)) {
+       return header.classList.add("delay--enter");
+        
+    }
+       
+    });
+  }
+  
+  function isElementInViewport (el) {
+    var rect = el.getBoundingClientRect();
+  
+    return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth) 
+    );
+  }
+  
+  if (window.addEventListener) {
+    addEventListener('DOMContentLoaded', checkForVisibility, false); 
+    addEventListener('load', checkForVisibility, false);
+    addEventListener('scroll', checkForVisibility, false);
+  }
