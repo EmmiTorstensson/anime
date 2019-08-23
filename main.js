57d4175c3aaf0bd521db3df8a0c98ec20489b095
emmitorstensson <emmitorstensson@hotmail.com>
2019-08-23 10:35:52 +0300

    emmi final

--- main.js
+++ main.js
@@ -5,15 +5,14 @@ const myAnimation = anime({
     targets: '.section-1-img',
     scale: 1.2,
     duration: 16000,
-    delay: function(el, i) { return i * 250 },    
+   /* delay: function(el, i) { return i * 250 },    */
     direction: 'alternate',
     easing: 'linear',
     loop: true,
 })
 
 const myTextAnimation = anime({
-    targets: '.section-1-text-wrapper .section-1-word',
-    translateY: [50,0],
+    targets: '.section-1-text .section-1-word',
     translateZ: 0,
     opacity: [0,1],
     easing: "easeOutExpo",
@@ -21,3 +20,16 @@ const myTextAnimation = anime({
     delay: function(el, i) {
         return 700*i;}
 })  
+
+
+/*
+const myLineAnimation = anime ({
+    targets: '.section-1-line-top',
+    translateY: [0, 100],
+    easing: "linear",
+    duration: 6000, 
+    delay: function(el, i) {
+        return 800*i;}
+})
+
+*/
