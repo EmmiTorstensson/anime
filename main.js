94c0bd2cea9b5a95fd43f81593c8096c3c815251
emmitorstensson <emmitorstensson@hotmail.com>
2019-08-23 10:44:32 +0300

    fixat krockar

--- main.js
+++ main.js
@@ -132,7 +132,6 @@ const myAnimation = anime({
     targets: '.section-1-img',
     scale: 1.2,
     duration: 16000,
-   /* delay: function(el, i) { return i * 250 },    */
     direction: 'alternate',
     easing: 'linear',
     loop: true,
